7f2a4859ed32039277f6d215a6822cc6fb9ffb12
András <dapunxz@hotmail.com>
2024-01-29 14:18:18 +0100

    To Do list written

--- SilentShuffle.lua
+++ SilentShuffle.lua
@@ -1,5 +1,12 @@
---Initial Version
+--Initial Version on 29 January 2024
 
+-- Initialization
 local versionNumber = "|cff00e5ff 1.0 |r"
 
 print("Silent Shuffle version".. versionNumber.. "loaded!")
+
+--To be Implemented
+-- * When entering Solo Shuffle, the disable chat command should be executed
+-- * When leaving Solo Shuffle, the enable chat command should be executed
+-- * Messages need to indicate successful execution of each state
+-- * Option to turn off the addon functions in game with a toggle command/UI box
