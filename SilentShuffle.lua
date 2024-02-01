9cfeaa8b0a4a8f1e7baa31e78beae771bad34f1e
András <dapunxz@hotmail.com>
2024-02-01 22:05:43 +0100

    Main features implemented

--- SilentShuffle.lua
+++ SilentShuffle.lua
@@ -1,24 +1,46 @@
 --Initial Version on 29 January 2024
 
 -- Initialization
-local versionNumber = "|cff00e5ff 1.0 |r"
+local addonName = "SilentShuffle"
+local silentShuffleTitle = "|cff00ff88Silent Shuffle|r"
+local GetAddOnMetadata = C_AddOns.GetAddOnMetadata
+local AddonVersion = GetAddOnMetadata(addonName, "Version")
+local setChatDisabled = C_SocialRestrictions.SetChatDisabled
+local IsRatedSoloShuffle = C_PvP.IsRatedSoloShuffle
 
-print("Silent Shuffle version".. versionNumber.. "loaded!")
-
+--print(silentShuffleTitle..": Silent Shuffle version".. versionNumber.. "successfully loaded")
+local welcomeMsg = "%s: Silent Shuffle version |cff00e5ff%s|r successfully loaded"
+print(string.format(welcomeMsg, silentShuffleTitle, AddonVersion))
 --To be Implemented
 -- * When entering Solo Shuffle, the disable chat command should be executed
--- * When leaving Solo Shuffle, the enable chat command should be executed
+-- * When leaving Solo Shuffle, the enable chat command should be executed - Done
 -- * Messages need to indicate successful execution of each state
 -- * Option to turn off the addon functions in game with a toggle command/UI box
+-- * Extra option: Disable Barrens chat :D
 
--- Create Addon Panel
+
+
+
 
 local silentShufflePanel = ...
 
+-- Create Addon Panel
 silentShufflePanel = {}
 
 silentShufflePanel.panel = CreateFrame("Frame", "SilentShufflePanel", UIParent);
-silentShufflePanel.panel.name = "SilentShufflePanel";
+silentShufflePanel.panel.name = "SilentShuffle";
 InterfaceOptions_AddCategory(silentShufflePanel.panel);
 
+-- Toggle the chat based on status if the player is in Solo Shuffle
+local function toggleChat()
+    if IsRatedSoloShuffle() then
+        setChatDisabled(true)
+        print(silentShuffleTitle..": In Shuffle - Chat Disabled")
+    else
+        setChatDisabled(false)
+        print(silentShuffleTitle..": Not in Shuffle - Chat Enabled")
+       -- print("Chat enabled - not in Solo Shuffle -- ".. isInShuffle())
+    end
+end
 
+toggleChat()
